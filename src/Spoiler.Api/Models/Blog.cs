df1974464a7770ee079dfc90699d2c65daf0e076
Abdulhameed-A <a.alshllal@hotmail.com>
2023-01-22 16:18:24 +0300

    added delete-endpoint, and made some modifications.

--- src/Spoiler.Api/Models/Blog.cs
+++ src/Spoiler.Api/Models/Blog.cs
@@ -4,11 +4,59 @@ namespace Spoiler.Api.Models
 {
     public class Blog
     {
-        [Required(ErrorMessage ="title is required")]
-        public string Title { get; set; }
+        public string? Id { get; set; }
+
+        [Required(ErrorMessage = "title is required")]
+        public string? Title { get; set; }
 
 
         [Required(ErrorMessage = "description is required")]
-        public string Description { get; set; }
+        public string? Description { get; set; }
+
+        public Blog()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+
+        public Blog(CreateBlogRequest create)
+            : this()
+        {
+            Title = create.Title;
+            Description = create.Description;
+        }
+
+        public static BlogDto Create(CreateBlogRequest create, List<Blog> blogs)
+        {
+            blogs ??= new List<Blog>();
+
+            var blog = new Blog(create);
+
+            blogs.Add(blog);
+
+            return new BlogDto(blog);
+        }
+    }
+
+    public class CreateBlogRequest
+    {
+        [Required(ErrorMessage = "title is required")]
+        public string? Title { get; set; }
+
+        [Required(ErrorMessage = "description is required")]
+        public string? Description { get; set; }
+    }
+
+    public class BlogDto
+    {
+        public string? Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+
+        public BlogDto(Blog blog)
+        {
+            Id = blog.Id;
+            Title = blog.Title;
+            Description = blog.Description;
+        }
     }
 }
